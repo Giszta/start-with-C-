7f2a8b0178773d5863fca6e234e918f9a0f31c1e
AdamGiszter <a.m.giszter@gmail.com>
2024-04-07 19:28:46 +0200

    modulus %

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -1,27 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-namespace Bool
-
+namespace Modulus
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int firstNum = 10;
+            int secondNum = 3;
 
-           bool value = true;
-
-           bool isMale = true;
-            Console.WriteLine(isMale);
-
-            isMale = false;
-            Console.WriteLine(isMale);
-
-
+            Console.WriteLine(firstNum/secondNum);
+            Console.WriteLine(firstNum%secondNum);
             Console.ReadLine();
         }
     }
