94908a1cbcf3776893fbae2ea6a50b740d412f03
AdamGiszter <a.m.giszter@gmail.com>
2024-04-07 20:04:03 +0200

    Console Input/Output

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -5,22 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Constants
-
+namespace ConsoleIO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            const int vat = 20;
-            const double percentVAT = vat / 100D;
-            //vat = 10;
-            Console.WriteLine(vat);
+            //Console.WriteLine("Hello my name is Adam!");
 
-            int balance = 1000;
-            Console.WriteLine(balance*percentVAT);
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            //Console.WriteLine("Hello " + name);
 
-            const string version = "v1.0";
+            Console.Write("Enter your age: ");
+            //string ageInput = Console.ReadLine();
+            //int age = Convert.ToInt32(ageInput);
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.Write("Your name is ");
+            //Console.Write(name);
+            //Console.Write(" and your age is ");
+            //Console.Write(age);
+            Console.WriteLine();
+            Console.WriteLine("Your name is " + name + " and your age is " + age);
+
+
 
             Console.ReadLine();
         }
